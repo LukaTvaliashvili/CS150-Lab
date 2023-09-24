9bc5425b71b2a29d81a73d1271c95d2ce92a4713
Luka <lukatvaliashvili@gmail.com>
2023-09-25 00:27:01 +0400

    lesson plan for 25th of September

---
+++ src/main/java/main/lesson20230925/LessonPlan.java
@@ -0,0 +1,26 @@
+package main.lesson20230925;
+
+public class LessonPlan {
+    public static void main(String[] args) {
+       /*
+       Portability?
+       Access modifiers?
+
+       Naming convention for identifiers
+            -restrictions
+            -camel case
+
+       Primitives & Type casting
+
+       Comments
+
+       Escape characters
+
+       Stack and Heap Memory
+
+
+
+        */
+
+    }
+}
