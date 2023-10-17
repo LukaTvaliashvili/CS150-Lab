beff88a95baeed364a4a31c22697c50e5c84f9ec
Luka <lukatvaliashvili@gmail.com>
2023-10-17 18:58:33 +0400

    add in-class problem description

---
+++ src/main/java/main/lesson20231016/InClassProblems.java
@@ -0,0 +1,23 @@
+package main.lesson20231016;
+
+public class InClassProblems {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static String twiceTheSmallerString(String s1, String s2){
+
+        return "";
+    }
+
+    /*
+    Given a String s1 and int n,
+    return a String made of the first and last n characters from the String.
+    The length of the input String will be at least n
+     */
+    public static String firstAndLastNCharacters(String s1, int n){
+
+        return "";
+    }
+}
