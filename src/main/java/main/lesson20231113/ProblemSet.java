0e57b20c300853afb2c715fc43d1cd7208838c22
Luka <luka.tvaliashvili@zailab.com>
2023-11-13 09:05:49 +0400

    problems for upcoming class

---
+++ src/main/java/main/lesson20231113/ProblemSet.java
@@ -0,0 +1,47 @@
+package main.lesson20231113;
+
+public class ProblemSet {
+
+    public static void main(String[] args) {
+
+    }
+
+    /*
+    Create a method that checks if the given word is a palindrome or not.
+    A palindrome is a word or is sequence of characters that reads the
+    same forward and backward. Here are some examples of palindromic words and phrases:
+
+    Palindrome Words:
+
+    radar
+    level
+    racecar
+    refer
+
+     */
+
+    /*
+    Write a program to calculate the factorial of a given number.
+     */
+
+    /*
+    Write a program to merge two arrays of the same size, sorted in ascending order.
+    Test the program with relevant values.
+     */
+
+    /*
+    Write a method that takes an int value and returns the number with its digits reversed.
+    For example, given the number 7631, the method should return 1367
+     */
+
+    /*
+    Write a method that expects an argument of type integer and "Positive", "Negative", and "Zero" otherwise.
+    Using this method check if the members of the given array, are positive, negative, or zero. Return a new array
+    containing these values.
+     */
+
+    /*
+    Create a method that calculates the value of a base raised to a positive integer exponent.
+    i.e, you need to create your own version of Math.pow() method.
+     */
+}
