79b8b5b474ca7ef48f2c83b37133fbb6012d91ea
Luka <luka.tvaliashvili@zailab.com>
2023-11-25 16:22:24 +0400

    problems for assignment five

---
+++ src/main/java/main/lesson20231127/Assignment.java
@@ -0,0 +1,65 @@
+package main.lesson20231127;
+
+public class Assignment {
+
+	public static void main(String[] args) {
+
+	}
+
+	/*
+	Given an array of integers, return true if it does not contain both 1 and 7.
+
+	no1And7([1, 2, 3]) → true
+	no1And7([1, 2, 3, 7]) → false
+	no1And7([2, 3, 7]) → true
+	 */
+
+	/*
+	Determine, whether a given year is a leap year or not.
+	The rules are as follows:
+
+	Years divisible by 4 are leap years,
+	but years divisible by 100 are not leap years,
+	but years divisible by 400 are leap years.
+
+	isLeapYear(2000) -> true
+    isLeapYear(2015) -> false
+	isLeapYear(2100) -> false
+	 */
+
+	/*
+	You will be given an array and a limit value.
+	You must check that all values in the array are below or equal to the limit value.
+	If they are, return true. Else, return false.
+	You can assume all values in the array are numbers.
+	 */
+
+
+	/*
+	In a small town the population is initial = 1000 at the beginning of a year.
+	The population regularly increases by 2 percent per year and moreover 50 new inhabitants per year come to live in the town.
+	How many years does the town need to see its population greater than or equal to target = 1200 inhabitants?
+
+	example:
+	calcYearsToTargetPopulation(1500, 5, 100, 5000) -> 15
+
+	Method signature should be as follows:
+
+	public static int calcYearsToTargetPopulation(int initial, double percent, int newInhabitants, int target) {}
+
+	 */
+
+	/*
+	Given a string indicating a range of letters, return a string which includes all the letters in that range, including the last letter.
+	Note that if the range is given in capital letters, return the string in capitals also!
+
+
+	"a-z" ➞ "abcdefghijklmnopqrstuvwxyz"
+	"h-o" ➞ "hijklmno"
+	"Q-Z" ➞ "QRSTUVWXYZ"
+	"J-J" ➞ "J"
+
+	Hint: while iterating from first to last char (i), use Character.toString(i) or cast it explicitly - (char) i
+	 */
+
+}
