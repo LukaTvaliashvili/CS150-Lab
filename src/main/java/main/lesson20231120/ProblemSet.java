5504fad77c868be62b529fa612d182e11482c0cd
Luka <luka.tvaliashvili@zailab.com>
2023-11-27 08:40:14 +0400

    problems for classwork

---
+++ src/main/java/main/lesson20231120/ProblemSet.java
@@ -0,0 +1,63 @@
+package main.lesson20231120;
+
+public class ProblemSet {
+
+	public static void main(String[] args) {
+
+	}
+
+	/*
+	You will be given an array and a limit value.
+	You must check that all values in the array are below or equal to the limit value.
+	If they are, return true. Else, return false.
+	You can assume all values in the array are numbers.
+	 */
+
+	/*
+	Implement the DNA replication logic, which involves creating a complementary strand for the given DNA sequence.
+	Use the following base pairing rules:
+
+	'A' pairs with 'T'
+	'T' pairs with 'A'
+	'C' pairs with 'G'
+	'G' pairs with 'C'
+
+	Output Complementary DNA Strand: Display the resulting complementary DNA strand formed during replication.
+
+	"ATCGTACGATCG" -> "TAGCATGCTAGC"
+
+	 */
+
+	/*
+    Given a string, capitalize the letters that occupy even indexes and odd indexes separately,
+    and return as shown below. Index 0 will be considered even.
+
+    For example, capitalize("abcdef") = ['AbCdEf', 'aBcDeF']. See test cases for more examples.
+
+    The input will be a lowercase string with no spaces.
+     */
+
+
+	 /*
+    Return the number of times that the string "code" appears anywhere in the given string,
+    except we'll accept any letter for the 'd', so "cope" and "cooe" count.
+
+    countCode("aaacodebbb") → 1
+    countCode("codexxcode") → 2
+    countCode("cozexxcope") → 2
+     */
+
+	 /*
+    Your task is to find the first element of an array that is not consecutive.
+    By not consecutive we mean not exactly 1 larger than the previous element of the array.
+
+    E.g. If we have an array [1,2,3,4,6,7,8] then 1 then 2 then 3 then 4 are all consecutive but 6 is not,
+    so that's the first non-consecutive number.
+
+    If the whole array is consecutive then return null.
+
+    The array will always have at least 2 elements1 and all elements will be numbers.
+    The numbers will also all be unique and in ascending order.
+    The numbers could be positive or negative and the first non-consecutive could be either too!
+     */
+}
