8a1f98974831ccde1ec666c8d9c6e0333b1ad812
Luka <lukatvaliashvili@gmail.com>
2023-12-18 14:37:25 +0400

    mini exam version 1

---
+++ src/main/java/main/lesson20231218/miniExam/version1/VersionOne.java
@@ -0,0 +1,30 @@
+package main.lesson20231218.miniExam.version1;
+
+public class VersionOne {
+
+    public static void main(String[] args) {
+
+    }
+
+    /*
+    Given a string and an int n, return a string made of n repetitions of the last n characters of the string.
+    You may assume that n is between 0 and the length of the string, inclusive.
+    Method name: repeatLast()
+     */
+
+    /*
+    Iterate over the members of the given array. If it contains an integer value presented as parameter
+    'value', square the elements before the value and leave the other elements unchanged.
+    There will be only one occurrence of element 'value'.
+
+    squareBefore({1,2,3,4,5,6,7}, 4) -> {1,4,9}
+
+     */
+
+    /*
+   Write a Java program that takes a string as input and returns an array of two strings. The first string should
+   contain characters from even indexes (considering index 0 as even) capitalized, and the second string should contain
+   characters from odd indexes capitalized. There will be no non-alphabetic characters.
+   Name of the method: capitalizeEvenOdd().
+     */
+}
---
+++ src/main/java/main/lesson20231218/miniExam/version2/VersionTwoTest.java
@@ -0,0 +1,105 @@
+package main.lesson20231218.miniExam.version2;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class VersionTwoTest {
+
+    @Test
+    public void testCountOccurrences1() {
+        assertEquals(1, VersionTwo.countOccurrences("abc hi ho", "hi"));
+    }
+
+    @Test
+    public void testCountOccurrences2() {
+        assertEquals(2, VersionTwo.countOccurrences("ABChi hi", "hi"));
+    }
+
+    @Test
+    public void testCountOccurrences3() {
+        assertEquals(3, VersionTwo.countOccurrences("hihihi", "hi"));
+    }
+
+    @Test
+    public void testCountOccurrences4() {
+        assertEquals(0, VersionTwo.countOccurrences("No occurrences here", "hello"));
+    }
+
+    @Test
+    public void testCountOccurrences5() {
+        assertEquals(3, VersionTwo.countOccurrences("hi hi hi", "hi"));
+    }
+
+    @Test
+    public void testSumAdjacentElements1() {
+        int[] inputArray = {1, 2, 3, 4, 5, 6};
+        int[] expectedArray = {3, 5, 7, 9, 11, 6};
+        assertArrayEquals(expectedArray, VersionTwo.sumAdjacentElements(inputArray));
+    }
+
+    @Test
+    public void testSumAdjacentElements2() {
+        int[] inputArray = {5, 10, 15, 20};
+        int[] expectedArray = {15, 25, 35, 20};
+        assertArrayEquals(expectedArray, VersionTwo.sumAdjacentElements(inputArray));
+    }
+
+    @Test
+    public void testSumAdjacentElements3() {
+        int[] inputArray = {-1, 0, 1, 2, 3};
+        int[] expectedArray = {-1, 1, 3, 5, 3};
+        assertArrayEquals(expectedArray, VersionTwo.sumAdjacentElements(inputArray));
+    }
+
+    @Test
+    public void testSumAdjacentElementsWithSingleElement() {
+        int[] inputArray = {42};
+        int[] expectedArray = {42};
+        assertArrayEquals(expectedArray, VersionTwo.sumAdjacentElements(inputArray));
+    }
+
+    @Test
+    public void testSumAdjacentElementsWithEmptyArray() {
+        int[] inputArray = {};
+        int[] expectedArray = {};
+        assertArrayEquals(expectedArray, VersionTwo.sumAdjacentElements(inputArray));
+    }
+
+    @Test
+    public void testConcatenateEvenOdd1() {
+        String inputString = "abcdefgh";
+        String expectedOutput = "acegbdfh";
+        assertEquals(expectedOutput, VersionTwo.concatenateEvenOdd(inputString));
+    }
+
+    @Test
+    public void testConcatenateEvenOdd2() {
+        String inputString = "xyz12345";
+        String expectedOutput = "x1y2z345";
+        assertEquals(expectedOutput, VersionTwo.concatenateEvenOdd(inputString));
+    }
+
+    @Test
+    public void testConcatenateEvenOdd3() {
+        String inputString = "JavaIsFun";
+        String expectedOutput = "JvaiFsnu";
+        assertEquals(expectedOutput, VersionTwo.concatenateEvenOdd(inputString));
+    }
+
+    @Test
+    public void testConcatenateEvenOddWithSingleCharacter() {
+        String inputString = "a";
+        String expectedOutput = "a";
+        assertEquals(expectedOutput, VersionTwo.concatenateEvenOdd(inputString));
+    }
+
+    @Test
+    public void testConcatenateEvenOddWithEmptyString() {
+        String inputString = "";
+        String expectedOutput = "";
+        assertEquals(expectedOutput, VersionTwo.concatenateEvenOdd(inputString));
+    }
+
+}
